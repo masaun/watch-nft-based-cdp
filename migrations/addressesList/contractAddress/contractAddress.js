a03efde00b52de306d675109d15ced6b7e83ab2e
masaun <masanoriuno@gmail.com>
2021-04-04 09:07:53 +0900

    Update deployed-addresses of 'WatchSignals' in ./migrations/addressesList

--- migrations/addressesList/contractAddress/contractAddress.js
+++ migrations/addressesList/contractAddress/contractAddress.js
@@ -2,5 +2,11 @@ module.exports = {
     "Mainnet": {
     },
     "Kovan": {
+        "Chainlink": {
+            "WatchSignals": {
+                "Oracle": "0x56dd6586DB0D08c6Ce7B2f2805af28616E082455",
+                "JobID": "77102eb8faae4532b9534d30749f54dc"
+            }
+        }
     }
 }
--- migrations/addressesList/tokenAddress/tokenAddress.js
+++ migrations/addressesList/tokenAddress/tokenAddress.js
@@ -2,5 +2,8 @@ module.exports = {
     "Mainnet": {
     },
     "Kovan": {
+        "Chainlink": {
+            "LINK Token": "0xa36085F69e2889c224210F603D836748e7dC0088"
+        }        
     }
 }
