2246ac21820d33a5e649ac232c7c73188c143b9e
masaun <masanoriuno@gmail.com>
2021-04-05 18:21:52 +0900

    [Arbitrum]: Update migration file by adding Arbitrum related-code to truffle-config.js

--- truffle-config.js
+++ truffle-config.js
@@ -3,17 +3,22 @@ require('dotenv').config();
 const HDWalletProvider = require('@truffle/hdwallet-provider');  // @notice - Should use new module.
 const mnemonic = process.env.MNEMONIC;
 
+/// Arbitrum
+const wrapProvider = require('arb-ethers-web3-bridge').wrapProvider
+const arbProviderUrl = 'http://localhost:8547/'
+
+
 module.exports = {
   networks: {
-    ropsten: {
-      provider: () => new HDWalletProvider(mnemonic, 'https://ropsten.infura.io/v3/' + process.env.INFURA_KEY),
-      network_id: '3',
-      gas: 4712388,
-      //gas: 4465030,          // Original
-      //gasPrice: 5000000000,  // 5 gwei (Original)
-      //gasPrice: 10000000000, // 10 gwei
-      gasPrice: 100000000000,  // 100 gwei
-      skipDryRun: true,        // Skip dry run before migrations? (default: false for public nets)
+    arbitrum: {
+      provider: function () {
+        // return wrapped provider:
+        return wrapProvider(
+          new HDWalletProvider(mnemonic, arbProviderUrl)
+        )
+      },
+      network_id: '*',
+      gasPrice: 0,
     },
     kovan: {
       provider: () => new HDWalletProvider(mnemonic, 'https://kovan.infura.io/v3/' + process.env.INFURA_KEY),
@@ -22,29 +27,6 @@ module.exports = {
       gasPrice: 5000000000, // 5 gwei
       //gasPrice: 100000000000,  // 100 gwei
       skipDryRun: true,     // Skip dry run before migrations? (default: false for public nets)
-    },
-    rinkeby: {
-      provider: () => new HDWalletProvider(mnemonic, "https://rinkeby.infura.io/v3/" + process.env.INFURA_KEY),
-      network_id: 4,
-      gas: 6000000,         // 2 times than before
-      gasPrice: 5000000000, // 5 gwei,
-      skipDryRun: true,     // Skip dry run before migrations? (default: false for public nets)
-      //from: process.env.DEPLOYER_ADDRESS
-    },
-    goerli: {
-      provider: () => new HDWalletProvider(mnemonic, "https://goerli.infura.io/v3/" + process.env.INFURA_KEY),
-      network_id: 5,
-      gas: 7500000,
-      gasPrice: 5000000000, // 5 gwei,
-      skipDryRun: true,     // Skip dry run before migrations? (default: false for public nets)
-      //from: process.env.DEPLOYER_ADDRESS
-    },
-    // main ethereum network(mainnet)
-    live: {
-      provider: () => new HDWalletProvider(mnemonic, "https://mainnet.infura.io/v3/" + process.env.INFURA_KEY),
-      network_id: 1,
-      gas: 5500000,
-      gasPrice: 2000000000 // 2 gwei
     },
     local: {
       host: '127.0.0.1',
