91f3693a1b71471d1ab127c01dd5468dc4de90e1
masaun <masanoriuno@gmail.com>
2021-04-05 12:31:12 +0900

    [Script]: Add transferLINKToExecutionContract() <- LINK balance of the WatchSignalsLuxuryWatchPriceOracle contract should has more than 0.1 LINK

--- scripts/scripts-kovan/WatchSignalsLuxuryWatchPriceOracle.script.js
+++ scripts/scripts-kovan/WatchSignalsLuxuryWatchPriceOracle.script.js
@@ -14,12 +14,14 @@ const tokenAddressList = require("../../migrations/addressesList/tokenAddress/to
 
 /// Artifact of smart contracts 
 const WatchSignalsLuxuryWatchPriceOracle = artifacts.require("WatchSignalsLuxuryWatchPriceOracle")
+const LinkTokenInterface = artifacts.require("LinkTokenInterface")
 
 /// ABIs
 let watchSignals_ABI = WatchSignalsLuxuryWatchPriceOracle.abi
 
 /// Contract addresses
 let WATCH_SIGNALS = WatchSignalsLuxuryWatchPriceOracle.address
+let LINK_TOKEN = tokenAddressList["Kovan"]["Chainlink"]["LINK Token"]
 
 
 /***
@@ -51,6 +53,7 @@ async function main() {
     await setUpSmartContracts();
 
     console.log("\n------------- Preparation for tests in advance -------------");
+    await transferLINKToExecutionContract()
 
     console.log("\n------------- Process of the WatchSignalsLuxuryWatchPriceOracle -------------");
     await requestPrice()
@@ -72,20 +75,39 @@ async function setUpSmartContracts() {
     console.log("Create the WatchSignalsLuxuryWatchPriceOracle contract instance");
     watchSignals = await WatchSignalsLuxuryWatchPriceOracle.at(WATCH_SIGNALS)
 
+    console.log("Create the LINK Token contract instance");
+    linkToken = await LinkTokenInterface.at(LINK_TOKEN)
+
     /// Logs (each deployed-contract addresses)
     console.log('=== WATCH_SIGNALS ===', WATCH_SIGNALS)
+    console.log('=== LINK_TOKEN ===', LINK_TOKEN)
 }
 
+async function transferLINKToExecutionContract() {
+    console.log("Transfer 1 LINK into the WatchSignalsLuxuryWatchPriceOracle contract");
+    const amount = web3.utils.toWei('1', 'ether')
+    const txReceipt = await linkToken.transfer(WATCH_SIGNALS, amount)
+
+    console.log("LINK balance of the WatchSignalsLuxuryWatchPriceOracle contract should has more than 0.1 LINK")
+    const _LinkBalance = await linkToken.balanceOf(WATCH_SIGNALS)
+    const LinkBalance = web3.utils.fromWei(String(_LinkBalance), 'ether')
+    console.log('=== LINK balance of the WatchSignalsLuxuryWatchPriceOracle contract ===', LinkBalance)    
+}
 
 async function requestPrice() {
     console.log("Request price");
 
-    /// [Note]: Need to have more than 1 LINK balance 
+    /// [Note]: Need to have more than 1 LINK balance of the WatchSignalsLuxuryWatchPriceOracle.sol
 
     /// Assign 
     const _oracle = contractAddressList["Kovan"]["Chainlink"]["WatchSignals"]["Oracle"]
     const _jobId = web3.utils.toHex(contractAddressList["Kovan"]["Chainlink"]["WatchSignals"]["JobID"])  /// [Note]: JobID is converted from string to bytes32
     const _refNumber = "RM1101"
+
+    console.log('=== _oracle ===', _oracle)
+    console.log('=== _jobId ===', _jobId)
+    console.log('=== _refNumber ===', _refNumber)
+
     let txReceipt = await watchSignals.requestPrice(_oracle, _jobId, _refNumber)
     //let txReceipt = await watchSignals.requestPrice(_oracle, _jobId, _refNumber, { from: deployer })
 }
